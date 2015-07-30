9c5e1e6257a723978b24ebe9d6fef38387ff65ea
Fukayanegi <fukayanegi1981@gmail.com>
2015-07-30 10:48:58 +0900

    add sample code of class

--- test.rb
+++ test.rb
@@ -5,3 +5,12 @@ a = ["a", "b", "c"]
 a.each do |obj| 
   puts obj.upcase
 end
+
+class MyClass
+  def say_hello
+    puts "Hello, this is instance method"
+  end
+end
+
+c = MyClass.new
+c.say_hello
