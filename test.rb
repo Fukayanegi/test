5e4ec62e5b5cbe9bbfff8f503be657b4ca964ff8
Fukayanegi <fukayanegi1981@gmail.com>
2015-08-15 06:01:47 +0900

    add something_special method

--- test.rb
+++ test.rb
@@ -5,3 +5,11 @@ a = ["a", "b", "c"]
 a.each do |obj| 
   puts obj.upcase
 end
+
+def a.something_special
+  self.each do |obj|
+    puts "This is special " + obj.upcase
+  end
+end
+
+a.something_special()
