e43670c1b61a3f72ce5f13bb87d109185697355a
Fukayanegi <fukayanegi1981@gmail.com>
2015-11-15 06:20:50 +0900

    split MyClass Closes #1

--- test.rb
+++ test.rb
@@ -1,15 +1,13 @@
+Dir[File.expand_path('../', __FILE__) << '/*.rb'].each do |file|
+  require file
+end
+
 puts "Hello, this is first commit"
 
 a = ["a", "b", "c"]
 
 a.each do |obj| 
   puts obj.upcase
-end
-
-class MyClass
-  def say_hello
-    puts "Hello, this is instance method"
-  end
 end
 
 c = MyClass.new
